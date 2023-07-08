1ff22e47478e80715aa6f87630afa7ca04725cb7
Anaya Bussey <anayabussey@gmail.com>
2023-07-07 21:50:53 -0400

    Committing

--- lifetracker-ui/src/components/Logo/Logo.jsx
+++ lifetracker-ui/src/components/Logo/Logo.jsx
@@ -2,6 +2,6 @@ import './Logo.css'
 
 export default function Logo() {
     return (
-        <img id="logo" src="http://localhost:5173/codepath.svg"/>
+        <img id="logo" src="https://life-tracker-frontend.onrender.com/codepath.svg"/>
     )
 }
